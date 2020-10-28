ad609f77691ecefb10b5c991d1240bdd636d1a94
Jochen Szaroletta <jochen.szaroletta@linde-le.com>
2020-10-28 13:57:56 +0100

    Code cleanup

--- LoraTTNMapper/include/settings.h
+++ LoraTTNMapper/include/settings.h
@@ -2,10 +2,6 @@
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <Effortless_SPIFFS.h>
-
-void load_settings();
-void save_settings();
 
 void saveConfiguration();
 void loadConfiguration();
