33af626c2d595aa794f7f01d75d547b1aa59c0e5
MrFlexi <szaroletta@hotmail.com>
2022-08-07 00:02:39 +0200

    Espressif downgrade to version 3.5.0

--- LoraTTNMapper/src/power.cpp
+++ LoraTTNMapper/src/power.cpp
@@ -207,8 +207,8 @@ void AXP192_power(pmu_power_t powerlevel)
     pmu.setPowerOutPut(AXP192_LDO2, AXP202_ON);  // Lora on T-Beam V1.0
     pmu.setPowerOutPut(AXP192_LDO3, AXP202_ON);  // Gps on T-Beam V1.0
     pmu.setPowerOutPut(AXP192_DCDC1, AXP202_ON); // OLED on T-Beam v1.0
-    //pmu.setChgLEDMode(AXP20X_LED_LOW_LEVEL);
-    pmu.setChgLEDMode(AXP20X_LED_BLINK_1HZ);
+    pmu.setChgLEDMode(AXP20X_LED_LOW_LEVEL);
+    //pmu.setChgLEDMode(AXP20X_LED_BLINK_1HZ);
     ESP_LOGI(TAG, "AXP power ON");
     break;
 
