e9fe1ed03e95c985b63b95493398590365b62d12
MrFlexi <szaroletta@hotmail.com>
2024-02-24 15:04:09 +0100

    na ja

--- LoraTTNMapper/DistanceSensoring/distance.py
+++ LoraTTNMapper/DistanceSensoring/distance.py
@@ -49,28 +49,38 @@ async def receive_data(websocket, path):
 
 def update_plot():
     points = calculate_points(gt_measurements)
+    last_point = points[-1]
     # Extract x and y data into separate lists using list comprehension
     x_data = [t[0] for t in points]
     y_data = [t[1] for t in points]
     plt.clf()  # Clear the current plot
     plt.scatter(x_data,y_data)  # Plot the points
-    plt.xlim(-4000, 4000)  # Set x-axis limits
-    plt.ylim(-4000, 4000)  # Set y-axis limits
+    plt.xlim(-1500, 1500)  # Set x-axis limits
+    plt.ylim(-1500, 1500)  # Set y-axis limits
     plt.xlabel('X')
     plt.ylabel('Y')
+    
+    x_line = []
+    y_line = []
+
+    # Add origin (0, 0) as the starting point of the line
+    x_line.append(0)
+    y_line.append(0)
+
+# Iterate through the data points and add their coordinates to the line
+    if last_point is not None:
+        print(last_point)
+        x_line.append(last_point[0])
+        y_line.append(last_point[1])
+
+        # Plot the line
+        plt.plot(x_line, y_line, color='red', linestyle='-', linewidth=2)
+    
     plt.title('X-Y Points')
     plt.grid(True)  # Show grid
     num_points = len(x_data)
-    plt.text(0, 3500, f'Number of points: {num_points}', fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
+    plt.text(0, 3500, f'n= {num_points}  w={last_point}', fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
     plt.pause(0.01)  # Pause to allow plot to update
-
-# Funktion zum Aktualisieren des Plots
-def update(frame):
-    
-    print(points)
-    for x, y in points:
-        ax.plot(x, y, 'bo') # 'bo' steht für blaue Kreise
-    return line,
 
 # Main function to connect to WebSocket server
 async def main():
